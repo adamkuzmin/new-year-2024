ccfa91dc98ef617840d2d644b65313ebf5b50166
Ilya Kuzmin <154247993+ilia-kuzmin-xyz@users.noreply.github.com>
2023-12-27 03:25:03 +0000

    added

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -1,23 +1,77 @@
 import styled from "styled-components";
 import roles from "../data/n.json";
+import { Button } from "antd";
+import { useState } from "react";
 
 interface CardProps {
   guest: string;
 }
 
+function Poem({ poemText }: any) {
+  // Разбиваем текст на абзацы по двойному переносу строк
+  const paragraphs = poemText.split("\n\n");
+
+  const formattedPoem = paragraphs.map(
+    (paragraph: any, paragraphIndex: any) => (
+      // Для каждого абзаца разбиваем его на строки
+      <div key={paragraphIndex} style={{ marginBottom: "16px" }}>
+        {paragraph.split("\n").map((line: any, lineIndex: any) => (
+          // Оборачиваем каждую строку в блок <div> с заданным стилем
+          <div
+            style={{
+              fontSize: "12px",
+              marginBottom: "0px",
+              fontWeight: "normal",
+            }}
+            key={lineIndex}
+          >
+            {line}
+          </div>
+        ))}
+      </div>
+    )
+  );
+
+  return <div>{formattedPoem}</div>;
+}
+
 const Card: React.FC<CardProps> = ({ guest }) => {
   const role = roles.find((role) => role.guest === guest);
+
+  const [isPoemVisible, setIsPoemVisible] = useState(false);
+
+  console.log("role?.poem.content", role?.poem.content);
 
   return (
     <Wrapper>
       <CardWrapper>
         <div>{role?.label}</div>
-        <div style={{ marginBottom: "30px" }}>{role?.style}</div>
+        {!isPoemVisible && (
+          <div style={{ marginBottom: "30px" }}>{role?.style}</div>
+        )}
 
-        <Thumbs>
-          <Thumb src={`/styles/${role?.id}/a.png`} />
-          <Thumb src={`/styles/${role?.id}/b.png`} />
-        </Thumbs>
+        {!isPoemVisible && (
+          <>
+            <Thumbs>
+              <Thumb src={`/styles/${role?.id}/a.png`} />
+              <Thumb src={`/styles/${role?.id}/b.png`} />
+            </Thumbs>
+          </>
+        )}
+
+        {isPoemVisible && <Poem poemText={role?.poem.content} />}
+
+        {!isPoemVisible && (
+          <div className="ant-btn" onClick={() => setIsPoemVisible(true)}>
+            Прочитать стишок {role?.poem.title}
+          </div>
+        )}
+
+        {isPoemVisible && (
+          <div className="ant-btn" onClick={() => setIsPoemVisible(false)}>
+            Назад
+          </div>
+        )}
       </CardWrapper>
     </Wrapper>
   );
@@ -55,7 +109,7 @@ const Thumb = styled.div<ThumbProps>`
 
 const CardWrapper = styled.div`
   width: calc(100% - 60px - 60px);
-  height: 300px;
+  min-height: 300px;
   background-color: white;
   border-radius: 20px;
   border: 10px solid #9f8cff;
@@ -73,6 +127,18 @@ const CardWrapper = styled.div`
     margin-bottom: 20px;
     font-size: 1.5rem;
     font-weight: bold;
+  }
+
+  &&&&&&& .ant-btn {
+    background-color: #9f8cff;
+    padding: 20px 10px 20px 10px;
+    border-radius: 20px;
+
+    &,
+    & * {
+      font-size: 0.8rem !important;
+      color: white !important;
+    }
   }
 `;
 
